79cdafdb4acbc8f3a9624940e8dbc0cab31f9029
ss-karthik <maniac.sskarthik@gmail.com>
2024-10-08 09:51:02 +0530

    ejs changes

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get("/enter", (req,res)=>{
-  res.render("login.ejs");
+  res.render("login");
 });
 
 app.post("/enter", (req,res)=>{
